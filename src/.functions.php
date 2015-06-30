96b097e846c974fa80d3fe571b9d95cc854d4da8
Stanislav Kiryukhin <korsar.zn@gmail.com>
2015-07-01 02:19:27 +0300

    Added base functions

--- src/.functions.php
+++ src/.functions.php
@@ -12,13 +12,23 @@
  * ----------------------------------------------
  */
 return [
+
+	// Operations
 	'abs' 	=> function($a) { return abs($a); },
-	'acos' 	=> function($a) { return acos($a); },
-	'acosh' => function($a) { return acosh($a); },
-	'asin'  => function($a) { return asin($a); },
+	'sqrt'  => function($a, $exp = 2) { return $exp > 2 ? pow($a, 1 / $exp) : sqrt($a); },
+	'ln' 	=> function($a) { return log($a); },
+	'lg' 	=> function($a) { return log10($a); },
+	'lb' 	=> function($a) { return log($a, 2); },
+	'log' 	=> function($a, $base = M_E) { return log($a, $base); },
 
-	'sqrt'  => function($a) { return sqrt($a); },
-
+	// Functions
 	'min'  => function($a) { return min(func_get_args()); },
-	'max'  => function($a) { return max(func_get_args()); }
+	'max'  => function($a) { return max(func_get_args()); },
+	'ceil' => function($a) { return ceil($a); },
+	'floor' => function($a) { return floor($a); },
+	'round' => function($a, $precision = 0) { return round($a, $precision); },
+
+	// Trigonometric functions
+	'sin' 	=> function($a) { return sin($a); },
+	'cos' => function($a) { return cos($a); }
 ];
